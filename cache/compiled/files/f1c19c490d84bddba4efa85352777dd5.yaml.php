13a50f10128d7a4ba28de142068a8181bcc91e03
Mike Crittenden <mikecrittenden@gmail.com>
2017-04-13 16:29:31 -0400

    Delete cache and logs and PHPStorm config

--- cache/compiled/files/f1c19c490d84bddba4efa85352777dd5.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'themes://notepad/notepad.yaml',
-    'modified' => 1491993152,
-    'data' => [
-        'enabled' => true
-    ]
-];
